6306c839c0b0f9e2f00cb373714a577e21d6561a
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-04-18 23:08:48 -0700

    2nd commit

---
+++ traversy_next_tutorial/src/pages/about.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <div>
+        <Head>
+            <title>About</title>
+            <meta />
+        </Head>
+      <h1>about</h1>
+    </div>
+  );
+};
+
+export default About;
