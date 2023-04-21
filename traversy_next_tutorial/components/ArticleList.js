12c8905573a5c1dbd48d9b655cb59a27e28dbb9f
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-04-20 23:55:16 -0700

    created articleItem component. Need to fix error

--- traversy_next_tutorial/components/ArticleList.js
+++ traversy_next_tutorial/components/ArticleList.js
@@ -1,10 +1,11 @@
 import articleStyles from "../src/styles/article.module.css";
+import ArticleItem from "./ArticleItem";
 
 const ArticleList = ({ articles }) => {
   return (
     <div className={articleStyles.grid}>
       {articles.map((article) => (
-        <h3>{article.title}</h3>
+        <ArticleItem article={article} />
       ))}
     </div>
   );
