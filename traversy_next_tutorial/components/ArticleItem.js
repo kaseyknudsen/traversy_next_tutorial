7c46a552fed2a4bd8938e3dc7d2d5e6aebd795b6
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-04-21 10:48:58 -0700

    fixed error

--- traversy_next_tutorial/components/ArticleItem.js
+++ traversy_next_tutorial/components/ArticleItem.js
@@ -2,12 +2,13 @@ import articleStyles from "../src/styles/article.module.css";
 import Link from "next/link";
 const ArticleItem = ({ article }) => {
   return (
-    <Link  href="/article/[id]" as={`/article/${article.id}`}>
-      <a className={articleStyles.card}>
-        <h3>{article.title} &rarr;</h3>
-
-        <p>{article}</p>
-      </a>
+    <Link
+      href="/article/[id]"
+      as={`/article/${article.id}`}
+      className={articleStyles.card}
+    >
+      <h3>{article.title} &rarr;</h3>
+      <p>{article.body}</p>
     </Link>
   );
 };
