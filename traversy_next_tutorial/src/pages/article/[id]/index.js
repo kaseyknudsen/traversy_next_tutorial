1c1888855c86997678253982559db93db26f8060
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-04-22 22:42:31 -0700

    using the API data

--- traversy_next_tutorial/src/pages/article/[id]/index.js
+++ traversy_next_tutorial/src/pages/article/[id]/index.js
@@ -1,5 +1,7 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
+import { articles } from "../../../../data";
+import { server } from "../../../../config";
 
 const article = ({ article }) => {
   //   const router = useRouter();
@@ -16,10 +18,9 @@ const article = ({ article }) => {
 
 //context allows us to get the ID of whatever is in the URL
 //getStaticProps is much faster because you can use it at build time with a static website
+
 export const getStaticProps = async (context) => {
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
-  );
+  const res = await fetch(`${server}/api/articles/${context.params.id}`);
 
   const article = await res.json();
 
@@ -31,15 +32,40 @@ export const getStaticProps = async (context) => {
 };
 
 export const getStaticPaths = async () => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/`);
+  const res = await fetch(`${server}/api/articles`);
 
   const articles = await res.json();
   const ids = articles.map((article) => article.id);
   const paths = ids.map((id) => ({ params: { id: id.toString() } }));
   return {
     paths,
-    fallback: false
+    fallback: false,
   };
 };
+// export const getStaticProps = async (context) => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+//   );
+
+//   const article = await res.json();
+
+//   return {
+//     props: {
+//       article,
+//     },
+//   };
+// };
+
+// export const getStaticPaths = async () => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/`);
+
+//   const articles = await res.json();
+//   const ids = articles.map((article) => article.id);
+//   const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+//   return {
+//     paths,
+//     fallback: false
+//   };
+// };
 
 export default article;
--- traversy_next_tutorial/src/pages/index.js
+++ traversy_next_tutorial/src/pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import ArticleList from "../../components/ArticleList";
+import {server} from "../../config";
 
 export default function Home({ articles }) {
   console.log(articles);
@@ -10,15 +11,13 @@ export default function Home({ articles }) {
         <title>WebDev Newz</title>
         <meta name="keywords" content="web development, programming" />
       </Head>
-      <ArticleList articles={articles}/>
+      <ArticleList articles={articles} />
     </div>
   );
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?_limit=6`
-  );
+  const res = await fetch(`${server}/api/articles`);
   const articles = await res.json();
 
   return {
@@ -27,3 +26,16 @@ export const getStaticProps = async () => {
     },
   };
 };
+
+// export const getStaticProps = async () => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts?_limit=6`
+//   );
+//   const articles = await res.json();
+
+//   return {
+//     props: {
+//       articles,
+//     },
+//   };
+// };
